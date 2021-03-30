aedf0ce42a3dcaf3fbdf9f793ca727a7e34edc75
Clotilde Connault <clotilde.connault@gmail.com>
2021-03-30 09:31:54 +0200

    test 2

--- src/main/java/be/technifutur/deploy/DeployApplication.java
+++ src/main/java/be/technifutur/deploy/DeployApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DeployApplication {
 
 	public static void main(String[] args) {
-		System.out.println("test git");
+		System.out.println("test git 2");
 		SpringApplication.run(DeployApplication.class, args);
 	}
 
