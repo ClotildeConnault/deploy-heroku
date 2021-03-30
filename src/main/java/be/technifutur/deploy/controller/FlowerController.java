9f2f85f81fecdd62a7329f3ce3be9afb84d21607
Clotilde Connault <clotilde.connault@gmail.com>
2021-03-30 14:56:38 +0200

    cross origin

--- src/main/java/be/technifutur/deploy/controller/FlowerController.java
+++ src/main/java/be/technifutur/deploy/controller/FlowerController.java
@@ -3,12 +3,14 @@ package be.technifutur.deploy.controller;
 import be.technifutur.deploy.dto.FlowerDTO;
 import be.technifutur.deploy.entities.Flower;
 import be.technifutur.deploy.services.FlowerService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping(path = "/flowers")
 public class FlowerController {
