52a2b21177880a3817ef48c286fa4bc16450a9f1
Clotilde Connault <clotilde.connault@gmail.com>
2021-03-30 09:59:51 +0200

    changement version jav

--- src/main/java/be/technifutur/deploy/DeployApplication.java
+++ src/main/java/be/technifutur/deploy/DeployApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DeployApplication {
 
 	public static void main(String[] args) {
-		System.out.println("test git 2");
+		System.out.println("test git 3");
 		SpringApplication.run(DeployApplication.class, args);
 	}
 
